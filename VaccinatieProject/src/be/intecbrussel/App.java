340ea887c150c686d59dfae077b7f9457b14ba7e
Tamara060890 <toma_tomova@hotmail.com>
2025-07-02 01:45:27 +0200

    Update App.java

--- VaccinatieProject/src/be/intecbrussel/App.java
+++ VaccinatieProject/src/be/intecbrussel/App.java
@@ -6,16 +6,12 @@ public class App {
     public static void main(String[] args) {
         AnimalShelter shelter = new AnimalShelter();
 
-        shelter.addAnimal(new Cat("Minoes", 4));
-        shelter.addAnimal(new Cat("Pussy", 7));
-        shelter.addAnimal(new Cat("Soni", 3));
-        shelter.addAnimal(new Monkey("George", 10));
-        shelter.addAnimal(new Monkey("Milli", 8));
-        shelter.addAnimal(new Monkey("Sam", 5));
-        shelter.addAnimal(new Dog("Bello", 8));
-        shelter.addAnimal(new Dog("Anny", 1));
-        shelter.addAnimal(new Dog("Bony", 9));
-        shelter.addAnimal(new Dog("Rex", 3));
+        shelter.addAnimal(new Cat("Minoes", 4, true, true));
+        shelter.addAnimal(new Monkey("George", 10, false, false));
+        shelter.addAnimal(new Dog("Bello", 6, false, true));
+        shelter.addAnimal(new Dog("Anny", 1, true, false));
+        shelter.addAnimal(new Cat("Soni", 3, false, false));
+        shelter.addAnimal(new Monkey("Milli", 8, true, true));
 
         System.out.println("--Alle dieren printen--");
         shelter.printAnimals();
@@ -26,7 +22,9 @@ public class App {
         System.out.println("--Dieren sorteren op dierennummer--");
         shelter.sortAnimals();
         System.out.println("--Aantal dieren in het asiel--");
-        System.out.println("Er zijn " + shelter.countAnimals() + " dieren.");
+        System.out.println(shelter.countAnimals());
+        System.out.println("--Alle dieren die niet schoon zijn--");
+        shelter.printAnimalsNotClean();
         System.out.println("--Zoek dier op naam--");
         System.out.println(shelter.findAnimal("Bello"));
         System.out.println("--Zoek dier met opgegeven diernummer--");
@@ -45,7 +43,7 @@ public class App {
         Animal animal = shelter.findAnimal(2);
         if (animal != null) {
             animal.vaccinateAnimal(Disease.FLUE);
-            System.out.println(animal.getAnimalNumber() + ". " + animal.getName() + " is gevaccineerd tegen FLUE.");
+            System.out.println(animal.getName() + " is gevaccineerd tegen FLUE.");
         }
 
         System.out.println("--Nr 1, 3 en 5 vaccineren tegen POLIO--");
@@ -54,12 +52,13 @@ public class App {
             Animal a = shelter.findAnimal(num);
             if (a != null) {
                 a.vaccinateAnimal(Disease.POLIO);
-                System.out.println(a.getAnimalNumber() + ". " + a.getName() + " is gevaccineerd tegen POLIO.");
+                System.out.println(a.getName() + " is gevaccineerd tegen POLIO.");
             }
         }
         System.out.println("--Print dieren die niet gevaccineerd zijn tegen FLUE--");
         shelter.printAnimalsNotVaccinated(Disease.FLUE);
         System.out.println("--Print dieren die niet gevaccineerd zijn tegen POLIO--");
         shelter.printAnimalsNotVaccinated(Disease.POLIO);
+
     }
 }
